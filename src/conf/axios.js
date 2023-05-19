8494094ca284b64381d24fc5a427864398d5e146
MatheoDev <matheo.deknuydt@gmail.com>
2023-05-19 11:50:35 +0200

    form feature & style conception

--- src/conf/axios.js
+++ src/conf/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'gcp-back.azurewebsites.net/api',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,79 @@
 import { createStore } from 'vuex'
+import data from './data.json'
 
 const store = createStore({
   state () {
     return {
-      questions: [],
+      input: {
+        firstname: '',
+        lastname: '',
+      },
+      form: {},
+      result: {},
+      step: 0,
+      loading: false,
     }
   },
   actions: {
-    fetchQuesttion ({ commit }) {
+    initMock({ commit }) {
+      // mock data
+      commit('setForm', data)
+    },
+    fetchQuesttion({ state, commit }) {
+      state.loading = true
       // fetch data from API
-
-      // this.$axios.get('/questions')
-
-      commit('setQuestions', ['test'])
+      this.$axios.post('/forms', state.input)
+        .then(response => {
+          // todo à verifier
+          commit('setForm', response.data)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+        .finally(() => {
+          // todo faire un loading
+          state.step = 1
+          state.loading = false
+        })
+    },
+    postQuestions({ state, commit }) {
+      state.loading = true
+      // post data to API
+      this.$axios.post(`/forms/${state.form.id}`, {
+          form: state.form,
+        })
+        .then(response => {
+          // todo à verifier
+          commit('setResult', response.data)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+        .finally(() => {
+          state.step = 2
+          state.loading = false
+        })
     }
   },
   mutations: {
-    setQuestions (state, questions) {
-      state.questions = questions
+    setForm (state, form) {
+      state.form = form
     },
-    addQuestion (state, question) {
-      state.questions.push(question)
+    setResult (state, result) {
+      state.result = result
+    },
+    setStep(state, step) {
+      state.step = step
+    },
+    clearAll(state) {
+      state.input = {
+        firstname: '',
+        lastname: '',
+      }
+      state.form = {}
+      state.result = {}
+      state.step = 0
+      state.loading = false
     }
   }
 })
