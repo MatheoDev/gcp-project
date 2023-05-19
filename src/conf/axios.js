2a758b986ff5195f42efcfafa258265eaebf541f
MatheoDev <matheo.deknuydt@gmail.com>
2023-05-19 14:20:04 +0200

    https

--- src/conf/axios.js
+++ src/conf/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://13.74.82.56/api',
+  baseURL: 'https://13.74.82.56/api',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
